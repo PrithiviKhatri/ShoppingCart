8377113188c8b89f158fd6a146f552699f24ed9b
prithivikhatri <prithivikhatri@gmail.com>
2015-11-18 00:58:43 -0600

    added spring aunthentication from DB , authorisation

--- src/main/java/mum/edu/shoppingcart/serviceimpl/CustomerServiceImpl.java
+++ src/main/java/mum/edu/shoppingcart/serviceimpl/CustomerServiceImpl.java
@@ -3,6 +3,8 @@ package mum.edu.shoppingcart.serviceimpl;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import mum.edu.shoppingcart.dao.CustomerDao;
@@ -21,6 +23,9 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	public void saveCustomer(Customer customer) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(customer.getCredential().getPassword());
+		customer.getCredential().setPassword(encodedPassword);
 		customerdao.saveCustomer(customer);
 	}
 
